a209f0fb6bb148586285733428e9fe3fc119b7f6
mikohan <angara99@gmail.com>
2022-01-28 12:00:36 +0300

    Fixed capitalized

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,16 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = '';
+  for (let word of str.split(' ')) {
+    const warr = word.split('');
+    warr[0] = warr[0].toUpperCase();
+    result += ' ' + warr.join('');
+  }
+  console.log(result);
+  return result.trim();
+}
+capitalize('look, it is working!');
 
 module.exports = capitalize;
